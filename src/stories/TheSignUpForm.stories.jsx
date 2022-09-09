a29c126ca6ec65eba187c7b2d0625b0f23935566
Linus Lövholm <linus.lovholm@forefront.se>
2022-09-09 15:46:16 +0200

    sdfsdf

--- src/stories/TheSignUpForm.stories.jsx
+++ src/stories/TheSignUpForm.stories.jsx
@@ -7,7 +7,12 @@ export default {
 	component: AnTIrESPOnsIVE,
 };
 
-const Template = (args) => <AnTIrESPOnsIVE {...args}>Hello world</AnTIrESPOnsIVE>;
+const Template = (args) => (
+	<AnTIrESPOnsIVE {...args}>
+		<b>Welcome!</b>
+		<p>Please (try to) sign up for Amplify. Good luck!</p>
+	</AnTIrESPOnsIVE>
+);
 
 export const Primary = Template.bind({});
 Primary.args = {};
